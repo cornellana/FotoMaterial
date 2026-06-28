6b3069f0ac004aae091f61853cc518251d4715ae
Francisco Cornellana <cornellana@mac.com>
2026-06-28 11:55:07 +0200

    Initial Commit

---
+++ FotoMaterial/FotoMaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotoMaterialApp.swift
+//  FotoMaterial
+//
+//  Created by Francisco on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct FotoMaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FotoMaterialTests/FotoMaterialTests.swift
@@ -0,0 +1,19 @@
+//
+//  FotoMaterialTests.swift
+//  FotoMaterialTests
+//
+//  Created by Francisco on 6/28/26.
+//
+
+import Testing
+@testable import FotoMaterial
+
+struct FotoMaterialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
